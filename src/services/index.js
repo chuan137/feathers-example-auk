21c64542778456ca28b18741d215fb6c4f1a1980
Chuan Miao <chuan137@gmail.com>
2017-02-20 20:54:35 +0100

    generate service orders/

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,10 @@
 
 const users = require('./users/users.service.js');
 
+const orders = require('./orders/orders.service.js');
+
 module.exports = function() {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
+  app.configure(orders);
 };
